71d0286b05d6ce04d2df469ddf3ed7aa7309762e
iamenochchirima <iamenochchirima@gmail.com>
2023-09-13 23:37:41 +0200

    Initial set up

---
+++ src/modernminds_frontend/src/App.tsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return <div className="bg-gray-900 text-gray-300">App</div>;
+};
+
+export default App;
